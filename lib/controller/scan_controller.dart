49f97164db37e89cbc6f9331e8ab48274cf0ce13
manansaipi <boben.manan5@gmail.com>
2024-02-09 19:39:30 +0700

    cameraview to detect an object, apply speact to text to navigate to camera view

---
+++ lib/controller/scan_controller.dart
@@ -0,0 +1,128 @@
+import 'package:camera/camera.dart';
+import 'package:flutter_vision/flutter_vision.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:vibration/vibration.dart';
+
+class ScanController extends GetxController {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    initCamera();
+    initTFLite();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  FlutterVision vision = FlutterVision();
+
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+  var detectedObject = "".obs;
+  List<Map<String, dynamic>> detectionResult = [
+    {
+      "box": [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+      ],
+      "tag": ""
+      // "polygons": List<Map<String, double>>: [{x:coordx, y:coordy}]
+    }
+  ];
+  var cameraCount = 0;
+
+  var x, y, w, h = 0.0;
+  var label = "";
+
+  var isCameraInitialized = false.obs;
+
+  final FlutterTts flutterTts = FlutterTts();
+
+  Future<void> speak(String text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.speak(text);
+  }
+
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      cameras = await availableCameras();
+
+      cameraController = CameraController(cameras[0], ResolutionPreset.max);
+      // await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 10 == 0) {
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
+      });
+      isCameraInitialized(true);
+      update();
+    } else {
+      print("Permission denied");
+    }
+  }
+
+  initTFLite() async {
+    print("LOAD THE MODEL !");
+
+    //flutter vision
+    await vision.loadYoloModel(
+      modelPath: 'assets/model/my_7yv8n.tflite',
+      labels: 'assets/model/mylabels.txt',
+      modelVersion: "yolov8",
+      quantization: false,
+      numThreads: 1,
+      useGpu: false,
+    );
+    print("MODEL LOAD SUCCESSFULLY");
+  }
+
+  objectDetector(CameraImage image) async {
+    // flutter vision
+    var result = await vision.yoloOnFrame(
+      bytesList: image.planes.map((plane) => plane.bytes).toList(),
+      imageHeight: image.height,
+      imageWidth: image.width,
+      iouThreshold: 0.4,
+      confThreshold: 0.4,
+      classThreshold: 0.5,
+    );
+    if (result.isNotEmpty) {
+      var box = detectionResult[0]['box'];
+      print("RESULTTTTTTTTTTTTTTTT:");
+      print(result);
+
+      detectionResult = result;
+      print(result);
+      //example result
+      // [{box: [0.0, 763.1640625, 357.9225158691406, 1116.581787109375, 0.5627957582473755], tag: Stop}]
+
+      speak(detectionResult[0]['tag']);
+      Vibration.vibrate();
+
+      label = detectionResult[0]['tag'];
+      x = box[0];
+      y = box[1];
+      w = box[2] - x;
+      h = box[3] - y;
+      update();
+    }
+    update();
+  }
+}
---
+++ lib/views/camera_view.dart
@@ -0,0 +1,84 @@
+import 'package:audiovision/controller/scan_controller.dart';
+import 'package:audiovision/views/object_detected.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:camera/camera.dart';
+
+class CameraView extends StatelessWidget {
+  const CameraView({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Camera View"),
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Get.back(); // Navigate back when the back button is pressed
+          },
+        ),
+      ),
+      body: GetBuilder<ScanController>(
+        init: ScanController(),
+        builder: (controller) {
+          return controller.isCameraInitialized.value
+              ? Stack(
+                  children: [
+                    CameraPreview(controller.cameraController),
+                    CustomPaint(
+                      // Use CustomPaint to draw the bounding box
+                      painter: BoundingBoxPainter(
+                          controller.detectionResult, context),
+                    ),
+                    DetectedObjectWidget(controller
+                        .detectionResult), // Display detected object info
+                    Text("data"),
+                  ],
+                )
+              : const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  //
+}
+
+
+
+// WIDGET FIR BUILD OBJECT
+// Widget _buildDetectedObject(List<Map<String, dynamic>> detectionResult) {
+//   if (detectionResult.isEmpty || detectionResult[0] == null) {
+//     // Return an empty container or some placeholder widget if detectionResult is empty or its first element is null
+//     return Container();
+//   }
+
+//   // Extracting box coordinates
+//   List<double> box = detectionResult[0]['box'].cast<double>();
+//   double x1 = box[0];
+//   double y1 = box[1];
+//   double x2 = box[2];
+//   double y2 = box[3];
+//   double confidence = box[4];
+
+//   // Extracting tag
+//   String tag = detectionResult[0]['tag'];
+//   String tag_confidence =
+//       '$tag - Confidence: ${confidence.toStringAsFixed(2)}'; // Format confidence as a string
+
+//   return Center(
+//     child: Container(
+//       padding: const EdgeInsets.all(8),
+//       decoration: BoxDecoration(
+//         color: Colors.black.withOpacity(0.6),
+//         borderRadius: BorderRadius.circular(8),
+//       ),
+//       child: Text(
+//         tag_confidence,
+//         style: TextStyle(color: Colors.white),
+//       ),
+//     ),
+//   );
+// }
