44e6c201d09f8aecd7806cac7cb0f0ea7db3570a
manansaipi <boben.manan5@gmail.com>
2024-02-11 01:20:23 +0700

    add user location to default starting point
    Signed-off-by: manansaipi <boben.manan5@gmail.com>

---
+++ lib/services/location_services.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:audiovision/services/user_location.dart';
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+  final StreamController<UserLocation> _locationStreamController =
+      StreamController<UserLocation>.broadcast();
+  Stream<UserLocation> get locationStream => _locationStreamController.stream;
+
+  LocationService() {
+    location.requestPermission().then((permissionStatus) {
+      if (permissionStatus == PermissionStatus.granted) {
+        location.onLocationChanged.listen((locationData) {
+          if (locationData != null) {
+            _locationStreamController.add(UserLocation(
+                latitude: locationData.latitude!,
+                longtitude: locationData.longitude!));
+          }
+        });
+      }
+    });
+  }
+
+  ///aasd
+  ///asd/
+  ///aasd
+  ///asd/
+  ///aasd
+  ///asd/
+  ///aasd
+  ///asd/
+
+  void dispose() => _locationStreamController.close();
+}
