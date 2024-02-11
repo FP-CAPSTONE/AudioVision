b546663ac1be9fa229fe9061d550301a6b2fa502
manansaipi <boben.manan5@gmail.com>
2024-02-11 11:04:57 +0700

    gesture detector to activate microphone

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:audiovision/mainAudio.dart';
 import 'package:audiovision/screens/select_screen.dart';
 import 'package:audiovision/views/camera_view.dart';
@@ -30,39 +32,56 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Appbar"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Container(
-        child: Center(
+    return GestureDetector(
+      onDoubleTap: () {
+        _isListening = false;
+        _text = "";
+        Vibration.vibrate();
+        _listen();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("My Appbar"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                "Say:\n"
+                "DOULBE TAP ANYWHERE ON THE SCREEN\n"
+                "\n"
+                "and Say:\n"
                 "\"Go to Camera View\",\n"
                 "\"Go to Map View\",\n"
                 "\"Go to Audio Guide\",\n"
                 "to navigate to another page",
                 style: TextStyle(fontSize: 16),
               ),
-              GestureDetector(
-                onTap: () {
-                  _isListening = false;
-                  _text = "";
-                  Vibration.vibrate();
-                  // speak("say something");
-                  // Get.to(CameraView());
-                  _listen();
-                },
-                child: Icon(
-                  _isListening ? Icons.mic : Icons.mic_none,
-                  size: 50,
-                  color: _isListening ? Colors.red : Colors.blue,
-                ),
-              ),
+              _isListening
+                  ? const Icon(
+                      Icons.mic,
+                      size: 50,
+                      color: Colors.red,
+                    )
+                  : SizedBox(),
+
+              //  MICROPHONE ICON TO ACTIVATE THE MICROPHONE
+              // GestureDetector(
+              //   onTap: () {
+              //     _isListening = false;
+              //     _text = "";
+              //     Vibration.vibrate();
+              //     // speak("say something");
+              //     // Get.to(CameraView());
+              //     _listen();
+              //   },
+              //   child: Icon(
+              //     _isListening ? Icons.mic : Icons.mic_none,
+              //     size: 50,
+              //     color: _isListening ? Colors.red : Colors.blue,
+              //   ),
+              // ),
               SizedBox(height: 10),
               Text(
                 _text,
@@ -90,7 +109,9 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {
           _isListening = true;
           _text = "Listening...";
+          print("Listening...");
         });
+
         _speech.listen(onResult: (result) {
           setState(() {
             _text = result.recognizedWords.toLowerCase();
@@ -110,12 +131,43 @@ class _HomeScreenState extends State<HomeScreen> {
               Get.to(MyAudioGuide());
               _isListening = false;
               _text = "";
+            } else {
+              // stop listening
+              _microphoneTimeout1();
             }
           });
         });
+        // stop listening
+        _microphoneTimeout2();
       } else {
         print('The user denied the use of speech recognition.');
       }
     }
+  }
+
+  // stop listening after 8 seconds
+  void _microphoneTimeout1() {
+    Timer(Duration(seconds: 8), () {
+      // Reset _isListening 8 seconds
+      setState(() {
+        _isListening = false;
+        _text = ""; // Clear the recognized text
+      });
+      print("Speech recognition timeout");
+    });
+  }
+
+  // stop listening if the user did not say anything
+  void _microphoneTimeout2() {
+    Timer(Duration(seconds: 5), () {
+      if (_text == "Listening...") {
+        // Reset _isListening if no speech is recognized after 5 seconds
+        setState(() {
+          _isListening = false;
+          _text = ""; // Clear the recognized text
+        });
+        print("Speech recognition timeout");
+      }
+    });
   }
 }
